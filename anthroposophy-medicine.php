7913acc79eec333ab4294d2ceaf7a8b47d82bb4e
mingxia <fu.shuaifeng@163.com>
2016-06-21 15:58:53 +0800

    Update anthroposophy-medicine.php

--- anthroposophy-medicine.php
+++ anthroposophy-medicine.php
@@ -41,7 +41,7 @@
     <?php include 'include/footer.php'; ?>
   </footer>
 </div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> 
+<script src="js/jquery.min.js"></script> 
 <script src="js/bootstrap.min.js"></script> 
 <script src="assets/js/docs.min.js"></script> 
 <script src="js/myjs.js"></script>
