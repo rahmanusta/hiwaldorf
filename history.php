9924f407a2ac212b9690ba503a044b8a05bdd21c
mingxia <fu.shuaifeng@163.com>
2016-06-21 16:09:39 +0800

    Update history.php

--- history.php
+++ history.php
@@ -42,7 +42,7 @@
 </div>
 
 <!-- Placed at the end of the document so the pages load faster --> 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> 
+<script src="js/jquery.min.js"></script> 
 <script src="js/bootstrap.min.js"></script> 
 <script src="assets/js/docs.min.js"></script> 
 <script src="js/myjs.js"></script>
